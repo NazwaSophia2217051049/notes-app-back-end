4919759a3696db90815e031c34f3577fdbc70c62
Nazwa <akunnazwa10@gmail.com>
2024-11-09 20:44:02 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,5 @@
+/* eslint-disable linebreak-style */
+const notes = [];
+// eslint-disable-next-line no-unused-vars, object-curly-spacing
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler} = require('./handler');
+module.exports = notes;
